ddf4ee549cc3d2633ccb6edc96ff88d26a733f5b
Sasha Koss <koss@nocorp.me>
2014-10-23 22:30:16 +0400

    Hello cruel world!

---
+++ rewire_test_helpers.js
@@ -0,0 +1,87 @@
+/**
+ * Set of helpers that helps easily inject dependencies using [rewire](rewire-link).
+ * [rewire-link]: https://github.com/jhnns/rewire
+ */
+var RewireTestHelpers = {
+  /**
+   * Injects specified dependencies and returns restore function.
+   *
+   * @param {object} target - any module required with rewire.
+   * @param {object} overrides - object, where key is private name and value is
+   * an override.
+   * @returns {function} restore function.
+   *
+   * @example
+   * beforeEach(function() {
+   *   this.restore = RewireTestHelpers.injectDependencies(SideNavigation, {
+   *     Link: DummyLink
+   *   });
+   * });
+   *
+   * afterEach(function() {
+   *   this.restore()
+   * });
+   */
+  injectDependencies: function(target, overrides) {
+    if (!target.__get__) {
+      throw 'Module must be required using rewire.';
+    }
+
+    var originals = {};
+    Object.keys(overrides).forEach(function(privateName) {
+      originals[privateName] = target.__get__(privateName);
+      target.__set__(privateName, overrides[privateName]);
+    });
+
+    return function() {
+      Object.keys(overrides).forEach(function(privateName) {
+        target.__set__(privateName, originals[privateName]);
+      });
+    };
+  },
+
+  /**
+   * Inject specified dependencies in beforeEach filter and restore them in
+   * afterEach.
+   *
+   * @param {Object} target - any module required with rewire.
+   * @param {Object} overrides - object, where key is private name and value is
+   * an override.
+   */
+  injectDependenciesFilter: function(target, overrides) {
+
+    beforeEach(function() {
+      this.restoreInjectedDependencies =
+        RewireTestHelpers.injectDependencies(target, overrides);
+    });
+
+    afterEach(function() {
+      this.restoreInjectedDependencies();
+    });
+  },
+
+  /**
+   * Injects specified dependencies, runs passed function and restores original
+   * version.
+   *
+   * @param {Object} target - any module required with rewire.
+   * @param {Object} overrides - object, where key is private name and value is
+   * an override.
+   * @param {Function} fn - function to be called.
+   *
+   * @example
+   * var SideNavigation = sinon.spy();
+   * RewireTestHelpers.rewired(AppLayout, {SideNavigation}, function() {
+   *   playground.render(<AppLayout />);
+   *   expect(SideNavigation).to.be.called;
+   * });
+   */
+  rewired: function(target, overrides, fn) {
+    var restore = RewireTestHelpers.injectDependencies(target, overrides);
+    fn();
+    restore();
+  }
+};
+
+module.exports = RewireTestHelpers;
+
